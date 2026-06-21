1d8bacc210182346dd876f522d1b17de71a122c6
zhanghuij <lcandxf503@sina.com>
2026-06-21 17:43:43 +0800

    feat：【init】提交数值扫描场景

--- Assets/Scripts/01_数值查找/ExtraValueScan.cs
+++ Assets/Scripts/01_数值查找/ExtraValueScan.cs
@@ -1,16 +1,50 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExtraValueScan : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Player _me;
+
+    public Text _hpText;
+    public Button _hurtBtn;
+    
+    public Text _speedText;
+    public Slider _speedSlider;
+    public Button _addSpeedBtn;
+    public Button _subSpeedBtn;
+    public float _speedDelta = 0.5f;
+    
+    private void Awake()
     {
+        _me = new Player(nameof(ExtraValueScan), 100);
         
+        _hurtBtn.onClick.AddListener(Hurt);
+        
+        _addSpeedBtn.onClick.AddListener(AddSpeed);
+        _subSpeedBtn.onClick.AddListener(SubSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddSpeed()
     {
+        _me.ChangeSpeed(_speedDelta);
+    }
+    
+    private void SubSpeed()
+    {
+        _me.ChangeSpeed(-_speedDelta);
+    }
+    
+    private void Update()
+    {
+        _hpText.text = $"hp: {_me.hp}";
+        _speedText.text = $"speed:{_me.speed}";
         
+        _speedSlider.value =  _me.speed;
+    }
+
+    private void Hurt()
+    {
+        _me.Hurt(5);
     }
 }
---
+++ Assets/Scripts/01_数值查找/Player.cs
@@ -0,0 +1,22 @@
+﻿public class Player
+{
+    public string name = string.Empty;
+    public uint hp = 0;
+    public uint mp = 0;
+    public float speed = 0;
+    public Player(string name, uint hp)
+    {
+        this.name = name;
+        this.hp = hp;
+    }
+    
+    public void Hurt(uint damage)
+    {
+        hp = hp - damage;
+    }
+
+    public void ChangeSpeed(float delta)
+    {
+        speed = speed + delta;
+    }
+}
